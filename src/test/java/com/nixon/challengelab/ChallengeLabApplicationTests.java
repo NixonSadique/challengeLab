b2be66ed5b5ab91c175bc878719f7e2467c1466d
NixonSadique <nixonsadique@gmail.com>
2026-06-29 12:37:46 +0200

    initial commit

---
+++ src/test/java/com/nixon/challengelab/ChallengeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nixon.challengelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
